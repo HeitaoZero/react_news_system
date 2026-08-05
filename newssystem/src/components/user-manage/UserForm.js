76f6457f2b79d30d2a61cba0e38a42171a7092bf
HeitaoZero <1569098942@qq.com>
2026-08-05 19:47:09 +0800

    2026/8/5--19:47

---
+++ newssystem/src/components/user-manage/UserForm.js
@@ -0,0 +1,176 @@
+import React from 'react'
+import { Form, Input, Modal, Select } from 'antd'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
+export default function UserForm(props) {
+    const [form] = Form.useForm();
+    const [regionList, setRegionList] = useState([]);
+    const [roleList, setRoleList] = useState([]);
+    const [isRoot, setIsRoot] = useState(false);
+
+    useEffect(() => {
+        const fetchOptions = async () => {
+            try {
+                // 并行请求，提高性能
+                const [regionRes, roleRes] = await Promise.all([
+                    axios.get('/api/regions'),
+                    axios.get('/api/roles')
+                ]);
+                // 使用 map 构建新数组，不修改原数据
+                const regionOptions = regionRes.data.map(item => ({
+                    value: item.id,
+                    label: item.title
+                }));
+                const roleOptions = roleRes.data.map(item => ({
+                    value: item.id,
+                    label: item.roleName
+                }));
+                setRegionList(regionOptions);
+                setRoleList(roleOptions);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchOptions()
+    }, [])
+
+    useEffect(() => {
+        if (isRoot) {
+            form.setFieldsValue({
+                region: null,
+            });
+        }
+    }, [isRoot, form]);
+    const createUser = async (values) => {
+        try {
+            const { roleName, ...data } = values;
+            await axios.post('/api/users', {
+                ...data,
+                roleState: true,
+                default: isRoot ? true : false,
+                region: isRoot ? "" : regionList.filter(item => item.value === values.region)[0].label,
+                roleId: roleName,
+            }).then(res => {
+                if (res.data.region === "") res.data.region = '全球'
+                props.setDataSource([...props.dataSource, { ...res.data, roleName: roleList.filter(item => item.value === roleName)[0].label }])
+
+                form.resetFields();
+                props.setIsVisible(false);
+                setIsRoot(false);
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    const handleCreate = async () => {
+        try {
+            const values = await form.validateFields()
+            createUser(values)
+        }
+        catch (error) {
+            console.log(error)
+        }
+    };
+    const clearAll = () => {
+        form.resetFields()
+    }
+
+    useEffect(() => {
+        form.setFieldsValue(props.dataSource.filter(data => data.id === props.chooseId)[0])
+        // console.log(props.dataSource.filter(data => data.id === props.chooseId)[0]?.roleId === 1)
+        if (props.dataSource.filter(data => data.id === props.chooseId)[0]?.roleId === 1) {
+            setIsRoot(true)
+        }
+    }, [props.chooseId, props.dataSource, form])
+
+    const handleUpdate = async (id) => {
+        try {
+            const userData = props.dataSource.find(data => data.id === id)
+            console.log(userData)
+            const values = await form.validateFields()
+            console.log(values)
+            if (userData.username === values.username && userData.region === values.region && userData.roleName === values.roleName && userData.password === values.password) {
+                props.setIsVisible(false)
+            } else {
+                props.dataSource.find(data => data.id === id).username = values.username
+                props.dataSource.find(data => data.id === id).region = values.region
+                props.dataSource.find(data => data.id === id).roleId = values.roleName
+                props.dataSource.find(data => data.id === id).password = values.password
+                props.dataSource.find(data => data.id === id).roleName = roleList.filter(item => item.value === values.roleName)[0].label
+                console.log(props.dataSource, values.roleName)
+                try {
+                    await axios.patch(`/api/users/${id}`, { username: values.username, region: values.region, roleId: values.roleName, password: values.password })
+
+                    props.setDataSource(props.dataSource)
+                    props.setIsVisible(false)
+                } catch (err) { console.log(err) }
+            }
+        }
+        catch (error) {
+            console.log(error)
+        }
+    };
+
+    return (
+        <Modal
+            title={props.title}
+            open={props.isVisible}
+            okText={props.title === '添加用户' ? 'Create' : 'Update'}
+            cancelText="Cancel"
+            okButtonProps={{ autoFocus: true, htmlType: 'submit' }}
+            onCancel={() => {
+                props.setIsVisible(false)
+                setIsRoot(false)
+                clearAll()
+                props.setChooseId?.(null)
+            }}
+            onOk={() => { props.title === "添加用户" ? handleCreate() : handleUpdate(props.chooseId) }}
+            destroyOnHidden
+            forceRender
+            modalRender={dom =>
+            (
+                <Form
+                    layout="vertical"
+                    form={form}
+                    name={props.title === "添加用户" ? "form_in_add_modal" : "form_in_edit_modal"}
+                    clearOnDestroy
+                    onFinish={(values) => { }}
+                >
+                    {dom}
+                </Form>
+            )}>
+
+            <Form.Item
+                name="username"
+                label="用户名"
+                rules={[{ required: true, message: 'Please input the title of collection!' }]}
+            >
+                <Input />
+            </Form.Item>
+            <Form.Item
+                name="password"
+                label="密码"
+                rules={[{ required: true, message: 'Please input the title of collection!' }]}
+            >
+                <Input type="textarea" />
+            </Form.Item>
+            <Form.Item
+                name="region"
+                label="区域"
+                rules={isRoot ? null : [{ required: true, message: 'Please input the title of collection!' }]}
+            >
+                <Select options={regionList} disabled={isRoot} />
+            </Form.Item>
+            <Form.Item
+                name="roleName"
+                label="角色"
+                rules={[{ required: true, message: 'Please input the title of collection!' }]}
+            >
+                <Select options={roleList} onChange={(value) => { value === 1 ? setIsRoot(true) : setIsRoot(false) }} />
+            </Form.Item>
+        </Modal>
+    )
+}
--- newssystem/src/views/sandbox/user-manage/UserList.js
+++ newssystem/src/views/sandbox/user-manage/UserList.js
@@ -1,7 +1,117 @@
 import React from 'react'
-
+import { Table, Switch, Button, Popconfirm } from 'antd';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons'
+import UserForm from "../../../components/user-manage/UserForm"
+import styles from '../user-manage/UserList.module.css'
 export default function UserList() {
+  const [dataSource, setDataSource] = useState([]);
+  const [isAddVisible, setIsAddVisible] = useState(false);
+  const [isUpdateVisible, setIsUpdateVisible] = useState(false);
+  const [chooseId, setChooseId] = useState(null);
+  const columns = [
+    {
+      title: '区域',
+      dataIndex: 'region',
+      key: 'region',
+      render: (region) => {
+        return <b>{region}</b>
+      },
+    },
+    {
+      title: '角色名称',
+      dataIndex: 'roleName',
+      key: 'roleName',
+    },
+    {
+      title: '用户名',
+      dataIndex: 'username',
+      key: 'username',
+    },
+    {
+      title: '状态',
+      dataIndex: 'roleState',
+      key: 'roleState',
+      render: (roleState, item) => {
+        return (
+          <Switch
+            checked={roleState}
+            onChange={() => { changeRoleState(item, roleState) }}
+          ></Switch>
+        )
+      },
+    },
+    {
+      title: '操作',
+      dataIndex: 'operation',
+      key: 'operation',
+      render: (operation, item) => {
+        return (<div className={styles.operate}>
+          <Popconfirm
+            title="Delete the task"
+            description="Are you sure to delete this task?"
+            cancelText="CANCEL"
+            cancelButtonProps={{ type: 'primary' }}
+            okButtonProps={{ type: 'primary' }}
+            okText="DELETE"
+            okType="danger"
+            placement="topRight"
+            onConfirm={() => { handleDeleteUser(item.id) }}
+          >
+            <Button danger icon={<DeleteOutlined />} shape="circle" ></Button>
+          </Popconfirm>
+          <Button type="primary" icon={<EditOutlined />} shape="circle" onClick={() => {
+            setIsUpdateVisible(true)
+            setChooseId(item.id)
+          }}></Button>
+        </div>
+        )
+      },
+    },
+  ];
+
+
+  const changeRoleState = async (item, roleState) => {
+    try {
+      dataSource.find(data => data.id === item.id).roleState = !roleState
+      setDataSource([...dataSource])
+      await axios.patch(`/api/users/${item.id}`, { roleState: !roleState })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  useEffect(() => {
+    const getUserList = async () => {
+      try {
+        const res = await axios.get('/api/users?_expand=role')
+        const dataSource = res.data.map(item => {
+          if (item.region === "") item.region = '全球'
+          return { ...item, roleName: item.role?.roleName || '暂无' }
+        })
+        setDataSource(dataSource)
+      } catch (error) {
+        console.log('获取用户列表失败', error)
+      }
+    };
+    getUserList();
+  }, [])
+
+  const handleDeleteUser = async (id) => {
+    try {
+      await axios.delete(`/api/users/${id}`).then(res => {
+        setDataSource(dataSource.filter(item => item.id !== id))
+      })
+    } catch (error) {
+      console.log('删除用户失败', error)
+    }
+  };
   return (
-    <div>UserList</div>
+    <div>
+      <Button type="primary" onClick={() => { setIsAddVisible(true) }}>添加用户</Button>
+      <Table dataSource={dataSource} columns={columns} rowKey="id" pagination={{ pageSize: 8 }} />;
+      <UserForm title={'添加用户'} isVisible={isAddVisible} setIsVisible={setIsAddVisible} setDataSource={setDataSource} dataSource={dataSource} />
+      <UserForm setChooseId={setChooseId} chooseId={chooseId} title={'修改用户'} isVisible={isUpdateVisible} setIsVisible={setIsUpdateVisible} setDataSource={setDataSource} dataSource={dataSource} />
+    </div>
   )
 }
